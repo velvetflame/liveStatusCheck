31f29eab9a6751c05a9897e7510e7654109febef
wosiwq <z_weipeng@126.com>
2023-05-25 17:20:52 +0800

    添加HEADERS，修改部分变量名

--- salt_gen.py
+++ salt_gen.py
@@ -5,23 +5,30 @@ import re
 
 
 def generator():
-    req = requests.request("GET", "https://api.bilibili.com/x/web-interface/nav")
-    con = ""
+    DEFAULT_HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Referer": "https://www.bilibili.com/7768957",
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+        "Origin": "https://space.bilibili.com",
+    }
+    req = requests.get("https://api.bilibili.com/x/web-interface/nav", headers=DEFAULT_HEADERS)
     if not req.ok:
         raise RequestException("请求数据失败")
     con = req.json()
-    imgUrl = con["data"]["wbi_img"]["img_url"]
-    subUrl = con["data"]["wbi_img"]["sub_url"]
+    img_url = con["data"]["wbi_img"]["img_url"]
+    sub_url = con["data"]["wbi_img"]["sub_url"]
     # 伪装成了url，提取其中文件名
     re_rule = r'wbi/(.*?).png'
-    imgKey = "".join(re.findall(re_rule, imgUrl))
-    subKey = "".join(re.findall(re_rule, subUrl))
+    img_key = "".join(re.findall(re_rule, img_url))
+    sub_key = "".join(re.findall(re_rule, sub_url))
 
-    n = imgKey + subKey  # 拼接两串值
-    array = list(n) # 拆分转arr
+    n = img_key + sub_key  # 拼接两串值
+    array = list(n)  # 拆分转arr
     order = [46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5,
-             49, 33, 9, 42, 19, 29, 28, 14, 39, 12,38, 41, 13, 37, 48, 7,
-             16, 24,55 ,40 ,61 ,26 ,17 ,0 ,1 ,60 ,51 ,30 ,4 ,22 ,25 ,54 ,
-             21 ,56 ,59 ,6 ,63 ,57 ,62 ,11 ,36 ,20 ,34 ,44 ,52]
-    salt = ''.join([array[i] for i in order])[:32] # 按照特定顺序混淆并取前32位
+             49, 33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7,
+             16, 24, 55, 40, 61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54,
+             21, 56, 59, 6, 63, 57, 62, 11, 36, 20, 34, 44, 52]
+    salt = ''.join([array[i] for i in order])[:32]  # 按照特定顺序混淆并取前32位
     return salt
+
